84812d329fc88f2c2c878f1e818be3dc76807219
Ben <benediktkaiser8@gmail.com>
2021-09-18 20:51:55 +0200

    (feat) Added Modal Component (#33)
    * Added Modal Boilerplate
    * Made Modal Overlay Blur

---
+++ web-app/stories/common/Modal.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import  { Modal } from "../../components/common/Modal";
+
+export default {
+    title: 'Common/Modal',
+    component: Modal,
+    argTypes: {
+        isOpen: true,
+        closeModal: { actions: "closeModal" },
+        openModal: { actions: "openModal" },
+    }
+};
+
+const Template = (args) => <div className="h-screen w-full bg-gray-300"> <p className="text-2xl font-bold text-center">Test Text</p> <Modal {...args}> Test Test </Modal></div>;
+
+export const Default = Template.bind({});
+Default.args = {
+    isOpen: true,
+};
