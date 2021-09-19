33053b49df53dc0708c574f485628c954de9bf19
Ben Kaiser <benediktkaiser8@gmail.com>
2021-09-19 13:03:16 +0200

    Added Storybook packages

--- web-app/.storybook/main.js
+++ web-app/.storybook/main.js
@@ -6,6 +6,7 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "storybook-dark-mode"
+    "@storybook/addon-postcss",
+    "storybook-tailwind-dark-mode"
   ]
 }
